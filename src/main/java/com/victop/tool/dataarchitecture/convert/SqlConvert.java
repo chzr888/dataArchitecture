fdc2ad4d781083a9558ffda192dd47c1badff8d5
chenzr <317053123@qq.com>
2014-07-01 11:59:44 +0800

    修改 数据转换时空值报错问题。

--- src/main/java/com/victop/tool/dataarchitecture/convert/SqlConvert.java
+++ src/main/java/com/victop/tool/dataarchitecture/convert/SqlConvert.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.victop.tool.dataarchitecture.Convert;
@@ -54,11 +57,12 @@ public class SqlConvert extends Convert {
 		StringBuilder placeStr = new StringBuilder();
 		ResultSetMetaData rsmd = rs.getMetaData();
 		ArrayList<String> fields = new ArrayList<String>();
+		Map<String, String> fieldMap = getTableType(tableName, H2Conn);
 		for (int i = 1; i <= rsmd.getColumnCount(); i++) {
 			if("SCHEMAVERGUID".equalsIgnoreCase(rsmd.getColumnLabel(i))){
 				continue;
 			}
-			fields.add(rsmd.getColumnLabel(i));
+			fields.add(rsmd.getColumnLabel(i).toLowerCase());
 			if( 0 != fieldStr.length()){
 				fieldStr.append(",");
 				placeStr.append(",");
@@ -72,13 +76,59 @@ public class SqlConvert extends Convert {
 		PreparedStatement pst = H2Conn.prepareStatement(sql);
 		while (rs.next()) {
 			for (int i = 0; i < fields.size(); i++) {
-				pst.setObject(i+1, rs.getObject(fields.get(i)));
+				String typeStr = fieldMap.get(fields.get(i));
+				if("INT".equalsIgnoreCase(typeStr) || "INTEGER".equalsIgnoreCase(typeStr)){
+					String string = rs.getString(fields.get(i));
+					if(null == string || "".equalsIgnoreCase(string) || "null".equalsIgnoreCase(string)){
+						pst.setObject(i+1, null);
+					}else{
+						pst.setObject(i+1, rs.getObject(fields.get(i)));
+					}
+				}else{
+					pst.setObject(i+1, rs.getObject(fields.get(i)));
+				}
 			}
 			pst.addBatch();
 		}
 		int[] msg = pst.executeBatch();
 		logger.info("{}",msg);
 		pst.close();
+	}
+	
+	/**
+	 * 返回表字段名类型
+	 * @param tableName
+	 * @param conn
+	 * @return
+	 * @throws Exception
+	 */
+	private Map<String, String> getTableType(String tableName, Connection conn) throws SQLException{
+		ResultSet rs = null;
+		Statement statement = null;
+		Map<String, String> columns = new HashMap<String, String>();
+		try {
+			statement = conn.createStatement();
+			rs = statement.executeQuery("select * from "+tableName + " WHERE 1=2");
+			ResultSetMetaData rsm = rs.getMetaData();
+			for (int i = 0; i < rsm.getColumnCount(); i++) {
+				columns.put(rsm.getColumnLabel(i+1).toLowerCase(), rsm.getColumnTypeName(i+1));
+			}
+		} catch (SQLException e) {
+			logger.error("{}",e);
+			throw e;
+		}finally{
+			if(null != rs){
+//				if(!rs.isClosed()){
+					rs.close();
+//				}
+			}
+			if(null != statement){
+//				if(!statement.isClosed()){
+					statement.close();
+//				}
+			}
+		}
+		return columns;
 	}
 	
 	/**
